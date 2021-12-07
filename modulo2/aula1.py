1bfd816b01618ab6822e58255cbaecde39f04d1d
Matheus Conde Nobre <matheuscnobre2010@hotmail.com>
2021-12-07 20:06:49 -0300

    atualização aula 1 - modulo 2

--- modulo2/aula1.py
+++ modulo2/aula1.py
@@ -1,1 +1,51 @@
-class Programa:
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.nome = "teste"
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} minutos - Likes: {vingadores.likes}')
+
+office = Serie('the office', 2004, 11)
+office.dar_like()
+print(f'Nome: {office.nome} - Ano: {office.ano} - Temporadas: {office.temporadas} Temporadas - Likes: {office.likes}')
